79459b0311cac482c65464d50e925e50de8b007f
Damian Olech <unova4@gmail.com>
2019-11-04 19:38:25 +0100

    fix bugs, add login, login page, edit index

--- src/main/java/Restaurant/Restaurant/Config/SecurityConfiguration.java
+++ src/main/java/Restaurant/Restaurant/Config/SecurityConfiguration.java
@@ -1,14 +1,11 @@
-package Restaurant.Restaurant.User.Config;
+package Restaurant.Restaurant.Config;
 
-import Restaurant.Restaurant.User.repository.UserRepository;
 import Restaurant.Restaurant.User.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -31,16 +28,22 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         @Override
         protected void configure (HttpSecurity http) throws Exception {
             http.csrf().disable();
-            http
-                    .authorizeRequests()
+            http.authorizeRequests()
                     .antMatchers("/user/**").authenticated()
                     .antMatchers("/admin/**").authenticated()
                     .anyRequest().permitAll()
                     .and()
                     .httpBasic()
                     .and()
-                    .formLogin().permitAll();
+                .formLogin()
+                    .loginPage("/login.html")
+                    .permitAll()
+                    .and()
+                    .logout()
+                    .permitAll();
         }
+
+
 
 
     private PasswordEncoder getPasswordEncoder () {
--- src/main/java/Restaurant/Restaurant/Restaurant/Dish.java
+++
@@ -1,7 +0,0 @@
-package Restaurant.Restaurant.Restaurant;
-
-public class Dish {
-
-    private Double Price;
-    private String Name;
-}
